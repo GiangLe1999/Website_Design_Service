dc0e86ffb7f14ead7617a540c41564dbc27c3802
Lê Thanh Giang <legiangbmt09@gmail.com>
2024-06-25 17:23:03 +0700

    Add nodemailer logic for contact form

--- actions/customer.action.tsx
+++ actions/customer.action.tsx
@@ -1,5 +1,6 @@
 "use server";
 
+import sendEmail from "@/lib/utils";
 import fs from "fs";
 import path from "path";
 
@@ -25,6 +26,21 @@ export const saveFormData = async (formData: any) => {
 
     // Write updated data back to the file
     fs.writeFileSync(dataFilePath, JSON.stringify(existingData, null, 2));
+
+    await sendEmail(
+      process.env.SMTP_MAIL_TO as string,
+      "KH RiverLee gửi form liên hệ",
+      `<h1>Khách hàng gửi form liên hệ tại website ${process.env.NEXT_PUBLIC_BASE_URL}:</h1>
+      <ul>
+        <li>Họ và tên: <b>${formData.full_name}</b></li>
+        <li>SĐT: <b>${formData.tel}</b></li>
+        <li>Email: <b>${formData.email}</b></li>
+        <li>Gói thiết kê: <b>${formData.type}</b></li>
+        <li>Tin nhắn: <b>${formData.mesage}</b></li>
+        <li>Thời gian gửi form: <b>${formData.created_at}</b></li>
+      </ul>
+      `
+    );
 
     console.log("Data saved successfully.");
   } catch (error) {
