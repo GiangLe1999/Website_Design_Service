d058c707522a48b4105301721a85964baf061903
Lê Thanh Giang <legiangbmt09@gmail.com>
2024-06-25 17:25:18 +0700

    Set up logic to write new contact to json file

--- actions/customer.action.tsx
+++ actions/customer.action.tsx
@@ -8,6 +8,21 @@ const dataFilePath = path.join(process.cwd(), "data", "form-data.json");
 
 export const saveFormData = async (formData: any) => {
   try {
+    await sendEmail(
+      process.env.SMTP_MAIL_TO as string,
+      "KH RiverLee gửi form liên hệ",
+      `<h1>Khách hàng gửi form liên hệ tại website ${process.env.NEXT_PUBLIC_BASE_URL}:</h1>
+      <ul>
+        <li>Họ và tên: <b>${formData.full_name}</b></li>
+        <li>SĐT: <b>${formData.tel}</b></li>
+        <li>Email: <b>${formData.email}</b></li>
+        <li>Gói thiết kê: <b>${formData.type}</b></li>
+        <li>Tin nhắn: <b>${formData.mesage}</b></li>
+        <li>Thời gian gửi form: <b>${formData.created_at}</b></li>
+      </ul>
+      `
+    );
+
     let existingData = [];
 
     // Check if the file exists
@@ -26,21 +41,6 @@ export const saveFormData = async (formData: any) => {
 
     // Write updated data back to the file
     fs.writeFileSync(dataFilePath, JSON.stringify(existingData, null, 2));
-
-    await sendEmail(
-      process.env.SMTP_MAIL_TO as string,
-      "KH RiverLee gửi form liên hệ",
-      `<h1>Khách hàng gửi form liên hệ tại website ${process.env.NEXT_PUBLIC_BASE_URL}:</h1>
-      <ul>
-        <li>Họ và tên: <b>${formData.full_name}</b></li>
-        <li>SĐT: <b>${formData.tel}</b></li>
-        <li>Email: <b>${formData.email}</b></li>
-        <li>Gói thiết kê: <b>${formData.type}</b></li>
-        <li>Tin nhắn: <b>${formData.mesage}</b></li>
-        <li>Thời gian gửi form: <b>${formData.created_at}</b></li>
-      </ul>
-      `
-    );
 
     console.log("Data saved successfully.");
   } catch (error) {
