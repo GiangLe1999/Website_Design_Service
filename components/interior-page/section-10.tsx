94eec152fd4d05b96b09aa65666c6085e88a3909
Lê Thanh Giang <100833182+GiangLe1999@users.noreply.github.com>
2024-10-03 23:03:04 +0700

    Responsive for steps on interior page

--- components/interior-page/section-10.tsx
+++ components/interior-page/section-10.tsx
@@ -21,11 +21,11 @@ const Section10: FC<Props> = (props): JSX.Element => {
   return (
     <section id={t('id')} className="relative bg-white pt-[100px]">
       <ContentContainer customClassName="max-w-[900px]">
-        <CustomHeading2 t={t} type_4 customClassname="text-center mb-12" />
+        <CustomHeading2 t={t} type_4 customClassname="text-center md:mb-12 mb-8" />
       </ContentContainer>
 
       <ContentContainer>
-        <div className="relative grid grid-cols-2 gap-52">
+        <div className="relative grid-cols-2 gap-52 md:grid hidden">
           {/* Left */}
           <div>
             <Reveal customClassname="mt-[200px] text-right">
@@ -89,8 +89,55 @@ const Section10: FC<Props> = (props): JSX.Element => {
           <Image
             src={steps_line}
             alt="Steps line"
-            className="absolute left-1/2 -translate-x-1/2 top-8 xl:block hidden"
+            className="absolute left-1/2 -translate-x-1/2 top-8 md:block hidden"
           />
+        </div>
+
+        <div className="md:hidden block space-y-[50px]">
+          <Reveal>
+            <p className="text-[#666666] md:text-xl text-lg uppercase mb-2">{t('step')} 1</p>
+            <p className="font-bold text-[#A57038] uppercase md:text-2xl text-xl mb-3">
+              {t('step_1.heading')}
+            </p>
+            <p className="text-[#666666] text-sm leading-7 mb-4">{t('step_1.description')}</p>
+            <Image src={step_1_img} alt={t('step_1.heading')} />
+          </Reveal>
+
+          <Reveal>
+            <p className="text-[#666666] md:text-xl text-lg uppercase mb-2">{t('step')} 2</p>
+            <p className="font-bold text-[#A57038] uppercase md:text-2xl text-xl mb-3">
+              {t('step_2.heading')}
+            </p>
+            <p className="text-[#666666] text-sm leading-7 mb-4">{t('step_2.description')}</p>
+            <Image src={step_2_img} alt={t('step_2.heading')} />
+          </Reveal>
+
+          <Reveal>
+            <p className="text-[#666666] md:text-xl text-lg uppercase mb-2">{t('step')} 3</p>
+            <p className="font-bold text-[#A57038] uppercase md:text-2xl text-xl mb-3">
+              {t('step_3.heading')}
+            </p>
+            <p className="text-[#666666] text-sm leading-7 mb-4">{t('step_3.description')}</p>
+            <Image src={step_3_img} alt={t('step_3.heading')} />
+          </Reveal>
+
+          <Reveal>
+            <p className="text-[#666666] md:text-xl text-lg uppercase mb-2">{t('step')} 4</p>
+            <p className="font-bold text-[#A57038] uppercase md:text-2xl text-xl mb-3">
+              {t('step_4.heading')}
+            </p>
+            <p className="text-[#666666] text-sm leading-7 mb-4">{t('step_4.description')}</p>
+            <Image src={step_4_img} alt={t('step_4.heading')} />
+          </Reveal>
+
+          <Reveal>
+            <p className="text-[#666666] md:text-xl text-lg uppercase mb-2">{t('step')} 5</p>
+            <p className="font-bold text-[#A57038] uppercase md:text-2xl text-xl mb-3">
+              {t('step_5.heading')}
+            </p>
+            <p className="text-[#666666] text-sm leading-7 mb-4">{t('step_5.description')}</p>
+            <Image src={step_5_img} alt={t('step_5.heading')} />
+          </Reveal>
         </div>
       </ContentContainer>
     </section>
