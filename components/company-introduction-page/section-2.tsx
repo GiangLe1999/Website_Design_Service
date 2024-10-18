3908f5b5fdeba235be0da32ca41ca317666e83cf
Lê Thanh Giang <100833182+GiangLe1999@users.noreply.github.com>
2024-10-18 17:13:57 +0700

    Update company page

--- components/company-introduction-page/section-2.tsx
+++ components/company-introduction-page/section-2.tsx
@@ -60,9 +60,9 @@ const Section2: FC<Props> = (props): JSX.Element => {
   const t = useTranslations('company_introduction_page.section_2');
 
   return (
-    <section id={t('id')} className="relative bg-[#0053d4] pt-[120px]">
+    <section id={t('id')} className="relative bg-[#0053d4] pt-[120px] overflow-hidden">
       <ContentContainer>
-        <div className="max-w-[600px] mb-14">
+        <div className="sm:max-w-[600px] mb-14">
           <CustomHeading2 t={t} customClassname="text-white" />
           <p className="text-white text-sm leading-7">{t('description')}</p>
         </div>
@@ -71,17 +71,17 @@ const Section2: FC<Props> = (props): JSX.Element => {
           <SectionSwiper />
         </Reveal>
 
-        <Reveal customClassname="pt-[160px] max-w-[500px] text-white">
+        <Reveal customClassname="pt-[160px] sm:max-w-[500px] text-white">
           <h3 className="font-bold text-3xl leading-snug mb-4">{t('sub_heading')}</h3>
           <p className="text-sm leading-7">{t('description')}</p>
         </Reveal>
 
         <div className="flex lg:flex-row flex-col items-center gap-20 px-5 sm:px-9 relative">
-          <Reveal customClassname="absolute grid place-items-center top-[15%] left-[-5%] after:bg-[linear-gradient(180deg,#009bff_0%,rgba(97,193,255,0)_100%)] after:opacity-[0.15] after:content-[''] after:absolute after:animate-pulse after:top-[-1.3rem] after:left-[-1.3rem] after:right-[-1.3rem] after:bottom-[-1.3rem] after:z-[1] after:rounded-full">
+          <Reveal customClassname="lg:grid hidden absolute grid place-items-center top-[15%] left-[-5%] after:bg-[linear-gradient(180deg,#009bff_0%,rgba(97,193,255,0)_100%)] after:opacity-[0.15] after:content-[''] after:absolute after:animate-pulse after:top-[-1.3rem] after:left-[-1.3rem] after:right-[-1.3rem] after:bottom-[-1.3rem] after:z-[1] after:rounded-full">
             <Image src={bottom_icon_1} alt="Icon 1" />
           </Reveal>
 
-          <Reveal customClassname="absolute grid place-items-center bottom-[30%] left-[25%] after:bg-[linear-gradient(180deg,#009bff_0%,rgba(97,193,255,0)_100%)] after:opacity-[0.15] after:content-[''] after:absolute after:animate-pulse after:top-[-1.3rem] after:left-[-1.3rem] after:right-[-1.3rem] after:bottom-[-1.3rem] after:z-[1] after:rounded-full">
+          <Reveal customClassname="lg:grid hidden absolute grid place-items-center bottom-[30%] left-[25%] after:bg-[linear-gradient(180deg,#009bff_0%,rgba(97,193,255,0)_100%)] after:opacity-[0.15] after:content-[''] after:absolute after:animate-pulse after:top-[-1.3rem] after:left-[-1.3rem] after:right-[-1.3rem] after:bottom-[-1.3rem] after:z-[1] after:rounded-full">
             <Image src={bottom_icon_2} alt="Icon 2" />
           </Reveal>
 
@@ -91,21 +91,21 @@ const Section2: FC<Props> = (props): JSX.Element => {
             <Image
               src={circles}
               alt="Circles"
-              className="absolute top-[-5%] left-[30%] z-[0] sm:w-[650px] w-[650px] max-w-none"
+              className="absolute top-[-5%] left-[30%] z-[0] sm:w-[650px] w-[500px] max-w-none"
             />
             <Image
               src={img}
               alt="Dịch vụ thiết kế website doanh nghiệp chuyên nghiệp"
-              className="md:w-[846px] sm:w-[750px] w-[640px] max-w-none relative z-[1]"
+              className="md:w-[846px] sm:w-[750px] w-[500px] max-w-none relative z-[1]"
               width={846}
               height={518}
             />
 
-            <Reveal customClassname="absolute grid place-items-center top-[15%] left-[24%] after:bg-[linear-gradient(180deg,#009bff_0%,rgba(97,193,255,0)_100%)] after:opacity-[0.15] after:content-[''] after:absolute after:animate-pulse after:top-[-1.3rem] after:left-[-1.3rem] after:right-[-1.3rem] after:bottom-[-1.3rem] after:z-[1] after:rounded-full">
+            <Reveal customClassname="lg:grid hidden absolute grid place-items-center top-[15%] left-[24%] after:bg-[linear-gradient(180deg,#009bff_0%,rgba(97,193,255,0)_100%)] after:opacity-[0.15] after:content-[''] after:absolute after:animate-pulse after:top-[-1.3rem] after:left-[-1.3rem] after:right-[-1.3rem] after:bottom-[-1.3rem] after:z-[1] after:rounded-full">
               <Image src={bottom_icon_3} alt="Icon 3" />
             </Reveal>
 
-            <Reveal customClassname="absolute grid place-items-center top-[-7%] right-[-27%] after:bg-[linear-gradient(180deg,#009bff_0%,rgba(97,193,255,0)_100%)] after:opacity-[0.15] after:content-[''] after:absolute after:animate-pulse after:top-[-1.3rem] after:left-[-1.3rem] after:right-[-1.3rem] after:bottom-[-1.3rem] after:z-[1] after:rounded-full">
+            <Reveal customClassname="lg:grid hidden absolute grid place-items-center top-[-7%] right-[-27%] after:bg-[linear-gradient(180deg,#009bff_0%,rgba(97,193,255,0)_100%)] after:opacity-[0.15] after:content-[''] after:absolute after:animate-pulse after:top-[-1.3rem] after:left-[-1.3rem] after:right-[-1.3rem] after:bottom-[-1.3rem] after:z-[1] after:rounded-full">
               <Image src={bottom_icon_4} alt="Icon 4" />
             </Reveal>
           </Reveal>
@@ -142,6 +142,17 @@ const SectionSwiper = () => {
         autoplay={{
           delay: 4000,
           disableOnInteraction: false,
+        }}
+        breakpoints={{
+          0: {
+            slidesPerView: 1,
+          },
+          700: {
+            slidesPerView: 2,
+          },
+          1100: {
+            slidesPerView: 3,
+          },
         }}
         pagination={{
           type: 'progressbar',
